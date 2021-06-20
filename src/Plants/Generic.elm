9193382a5763022fff347ad7d39dc71868bb4648
rvarun011 <varunrajput11@zoho.com>
2021-06-20 14:05:22 +0530

    moved old commented code to bottom

--- src/Plants/Generic.elm
+++ src/Plants/Generic.elm
@@ -5,65 +5,6 @@ import GraphicSVG.EllieApp exposing (graphicsApp)
 
 
 
--- using Records
--- type alias Trunk =
---     { x1 : Float
---     , y1 : Float
---     , x2 : Float
---     , y2 : Float
---     }
--- type alias Leaf =
---     { width : Float
---     , height : Float
---     , radius : Float
---     , angle : Float
---     }
--- initTrunk : Trunk
--- initTrunk =
---     { x1 = 0
---     , y1 = -20
---     , x2 = 0
---     , y2 = 100
---     }
--- initLeaf : Leaf
--- initLeaf =
---     { width = 10
---     , height = 20
---     , radius = 20
---     , angle = 120
---     }
--- viewTrunk : Trunk -> Shape msg
--- viewTrunk trunk =
---     line ( trunk.x1, trunk.y1 ) ( trunk.x2, trunk.y2 ) |> outlined (solid 1) darkBrown
--- viewLeaves : Leaf -> Float -> Float -> Shape msg
--- viewLeaves leaf cx cy =
---     group
---         [ roundedRect leaf.width leaf.height leaf.radius
---             |> filled green
---             |> rotate (degrees leaf.angle)
---             |> move ( 10 - cx, cy )
---         , roundedRect leaf.width leaf.height leaf.radius
---             |> filled green
---             |> rotate (degrees leaf.angle)
---             |> move ( 10 - cx, cy )
---             |> mirrorX
---         ]
--- viewPlant : Trunk -> Leaf -> Float -> Float -> Shape msg
--- viewPlant trunk leaf cx cy =
---     group
---         [ viewTrunk trunk
---         , if cy == distanceFormula trunk.x1 trunk.y1 trunk.x2 trunk.y2 then
---             group []
---           else
---             group
---                 [ viewLeaves leaf cx cy
---                 , viewPlant
---                     trunk
---                     { leaf | width = leaf.width - 1, height = leaf.height - 1, radius = leaf.radius - 1 }
---                     (cx + 1)
---                     (cy + 20)
---                 ]
---         ]
 -- Pure Functions
 
 
@@ -133,3 +74,65 @@ view =
 
 main =
     graphicsApp { view = view }
+
+
+
+-- Using Records
+-- type alias Trunk =
+--     { x1 : Float
+--     , y1 : Float
+--     , x2 : Float
+--     , y2 : Float
+--     }
+-- type alias Leaf =
+--     { width : Float
+--     , height : Float
+--     , radius : Float
+--     , angle : Float
+--     }
+-- initTrunk : Trunk
+-- initTrunk =
+--     { x1 = 0
+--     , y1 = -20
+--     , x2 = 0
+--     , y2 = 100
+--     }
+-- initLeaf : Leaf
+-- initLeaf =
+--     { width = 10
+--     , height = 20
+--     , radius = 20
+--     , angle = 120
+--     }
+-- viewTrunk : Trunk -> Shape msg
+-- viewTrunk trunk =
+--     line ( trunk.x1, trunk.y1 ) ( trunk.x2, trunk.y2 ) |> outlined (solid 1) darkBrown
+-- viewLeaves : Leaf -> Float -> Float -> Shape msg
+-- viewLeaves leaf cx cy =
+--     group
+--         [ roundedRect leaf.width leaf.height leaf.radius
+--             |> filled green
+--             |> rotate (degrees leaf.angle)
+--             |> move ( 10 - cx, cy )
+--         , roundedRect leaf.width leaf.height leaf.radius
+--             |> filled green
+--             |> rotate (degrees leaf.angle)
+--             |> move ( 10 - cx, cy )
+--             |> mirrorX
+--         ]
+-- viewPlant : Trunk -> Leaf -> Float -> Float -> Shape msg
+-- viewPlant trunk leaf cx cy =
+--     group
+--         [ viewTrunk trunk
+--         , if cy == distanceFormula trunk.x1 trunk.y1 trunk.x2 trunk.y2 then
+--             group []
+--           else
+--             group
+--                 [ viewLeaves leaf cx cy
+--                 , viewPlant
+--                     trunk
+--                     { leaf | width = leaf.width - 1, height = leaf.height - 1, radius = leaf.radius - 1 }
+--                     (cx + 1)
+--                     (cy + 20)
+--                 ]
+--         ]
